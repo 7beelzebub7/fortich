2522a0f9055a808a16976e5a97ee5caa52c02e98
7beelzebub7 <rc0870984@gmail.com>
2025-11-06 20:55:40 -0500

    Updates

--- src/pages/Inicio.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Inicio(){
-    return(
-        <>
-        <h1>pagina de inicio</h1>
-        </>
-    );
-}
--- src/pages/Portafolio.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Portafolio(){
-    return(
-        <>
-        <h1>pagina de portafolio</h1>
-        </>
-    );
-}
